d1a400835fe44c1dd93847cd27fd1baa6811ed33
kntt32 <153593861+kntt32@users.noreply.github.com>
2025-01-11 18:46:55 +0900

    データの追加

--- asm/src/instruction/instruction_database.rs
+++ asm/src/instruction/instruction_database.rs
@@ -2,6 +2,18 @@ use super::*;
 
 /// Instruction list
 pub static INSTRUCTION_LIST: &[Instruction] = &[
+    ADC_AL_IMM8,
+    ADC_AX_IMM16,
+    ADC_EAX_IMM32,
+    ADC_RAX_IMM32,
+    ADC_RM8_IMM8,
+    ADC_RM16_IMM16,
+    ADC_RM32_IMM32,
+    ADC_RM64_IMM32,
+    ADC_RM16_IMM8,
+    ADC_RM32_IMM8,
+    ADC_RM64_IMM8,
+    ADC_RM8_REG8,
     PUSH_R64,
     PUSH_RM64,
     PUSH_IMM64,
@@ -13,6 +25,194 @@ pub static INSTRUCTION_LIST: &[Instruction] = &[
     NEAR_RET,
     NEAR_CALL,
 ];
+
+/// ADC AL, imm8    14 ib
+const ADC_AL_IMM8: Instruction = Instruction {
+    encoding: EncodingRule {
+        opecode: SVec::from_raw([0x14, 0x00, 0x00], 1),
+        modrm: None,
+        imm: Some(ImmRule::Ib),
+        opecode_register: None,
+        default_operand_size: OperandSize::Od,
+    },
+    expression: Expression {
+        mnemonic: "adc",
+        operands: [Some(OperandType::Al), Some(OperandType::Imm8)],
+    },
+};
+
+/// ADC AX, imm16   15 iw
+const ADC_AX_IMM16: Instruction = Instruction {
+    encoding: EncodingRule {
+        opecode: SVec::from_raw([0x15, 0x00, 0x00], 1),
+        modrm: None,
+        imm: Some(ImmRule::Iw),
+        opecode_register: None,
+        default_operand_size: OperandSize::Od,
+    },
+    expression: Expression {
+        mnemonic: "adc",
+        operands: [Some(OperandType::Ax), Some(OperandType::Imm16)],
+    },
+};
+
+// ADC EAX, imm32   15 id
+const ADC_EAX_IMM32: Instruction = Instruction {
+    encoding: EncodingRule {
+        opecode: SVec::from_raw([0x15, 0x00, 0x00], 1),
+        modrm: None,
+        imm: Some(ImmRule::Id),
+        opecode_register: None,
+        default_operand_size: OperandSize::Od,
+    },
+    expression: Expression {
+        mnemonic: "adc",
+        operands: [Some(OperandType::Eax), Some(OperandType::Imm32)],
+    },
+};
+
+// ADC RAX, imm32   15 id
+const ADC_RAX_IMM32: Instruction = Instruction {
+    encoding: EncodingRule {
+        opecode: SVec::from_raw([0x15, 0x00, 0x00], 1),
+        modrm: None,
+        imm: Some(ImmRule::Id),
+        opecode_register: None,
+        default_operand_size: OperandSize::Od,
+    },
+    expression: Expression {
+        mnemonic: "adc",
+        operands: [Some(OperandType::Rax), Some(OperandType::Imm32)],
+    },
+};
+
+// ADC reg/mem8, imm8   80 /2 ib
+const ADC_RM8_IMM8: Instruction = Instruction {
+    encoding: EncodingRule {
+        opecode: SVec::from_raw([0x80, 0x00, 0x00], 1),
+        modrm: Some(ModRmRule::Dight(2)),
+        imm: Some(ImmRule::Ib),
+        opecode_register: None,
+        default_operand_size: OperandSize::Od,
+    },
+    expression: Expression {
+        mnemonic: "adc",
+        operands: [Some(OperandType::Rm8), Some(OperandType::Imm8)],
+    },
+};
+
+// ADC reg/mem16, imm16     81 /2 iw
+const ADC_RM16_IMM16: Instruction = Instruction {
+    encoding: EncodingRule {
+        opecode: SVec::from_raw([0x81, 0x00, 0x00], 1),
+        modrm: Some(ModRmRule::Dight(2)),
+        imm: Some(ImmRule::Iw),
+        opecode_register: None,
+        default_operand_size: OperandSize::Od,
+    },
+    expression: Expression {
+        mnemonic: "adc",
+        operands: [Some(OperandType::Rm16), Some(OperandType::Imm16)],
+    },
+};
+
+// ADC reg/mem32, imm32     81 /2 id
+const ADC_RM32_IMM32: Instruction = Instruction {
+    encoding: EncodingRule {
+        opecode: SVec::from_raw([0x81, 0x00, 0x00], 1),
+        modrm: Some(ModRmRule::Dight(2)),
+        imm: Some(ImmRule::Id),
+        opecode_register: None,
+        default_operand_size: OperandSize::Od,
+    },
+    expression: Expression {
+        mnemonic: "adc",
+        operands: [Some(OperandType::Rm32), Some(OperandType::Imm32)],
+    },
+};
+
+// ADC reg/mem64, imm32     81 /2 id
+const ADC_RM64_IMM32: Instruction = Instruction {
+    encoding: EncodingRule {
+        opecode: SVec::from_raw([0x81, 0x00, 0x00], 1),
+        modrm: Some(ModRmRule::Dight(2)),
+        imm: Some(ImmRule::Id),
+        opecode_register: None,
+        default_operand_size: OperandSize::Od,
+    },
+    expression: Expression {
+        mnemonic: "adc",
+        operands: [Some(OperandType::Rm64), Some(OperandType::Imm32)],
+    },
+};
+
+// ADC reg/mem16, imm8      83 /2 ib
+const ADC_RM16_IMM8: Instruction = Instruction {
+    encoding: EncodingRule {
+        opecode: SVec::from_raw([0x83, 0x00, 0x00], 1),
+        modrm: Some(ModRmRule::Dight(2)),
+        imm: Some(ImmRule::Ib),
+        opecode_register: None,
+        default_operand_size: OperandSize::Od,
+    },
+    expression: Expression {
+        mnemonic: "adc",
+        operands: [Some(OperandType::Rm16), Some(OperandType::Imm8)],
+    },
+};
+
+// ADC reg/mem32, imm8      83 /2 ib
+const ADC_RM32_IMM8: Instruction = Instruction {
+    encoding: EncodingRule {
+        opecode: SVec::from_raw([0x83, 0x00, 0x00], 1),
+        modrm: Some(ModRmRule::Dight(2)),
+        imm: Some(ImmRule::Ib),
+        opecode_register: None,
+        default_operand_size: OperandSize::Od,
+    },
+    expression: Expression {
+        mnemonic: "adc",
+        operands: [Some(OperandType::Rm32), Some(OperandType::Imm8)],
+    },
+};
+
+// ADC reg/mem64, imm8      83 /2 ib
+const ADC_RM64_IMM8: Instruction = Instruction {
+    encoding: EncodingRule {
+        opecode: SVec::from_raw([0x83, 0x00, 0x00], 1),
+        modrm: Some(ModRmRule::Dight(2)),
+        imm: Some(ImmRule::Ib),
+        opecode_register: None,
+        default_operand_size: OperandSize::Od,
+    },
+    expression: Expression {
+        mnemonic: "adc",
+        operands: [Some(OperandType::Rm64), Some(OperandType::Imm8)],
+    },
+};
+
+// ADC reg/mem8, reg8   10 /r
+const ADC_RM8_REG8: Instruction = Instruction {
+    encoding: EncodingRule {
+        opecode: SVec::from_raw([0x10, 0x00, 0x00], 1),
+        modrm: Some(ModRmRule::R),
+        imm: None,
+        opecode_register: None,
+        default_operand_size: OperandSize::Od,
+    },
+    expression: Expression {
+        mnemonic: "adc",
+        operands: [Some(OperandType::Rm8), Some(OperandType::R8)],
+    },
+};
+
+// ADC reg/mem16, reg1611 /rAdd reg16 to reg/mem16 + CF.
+// ADC reg/mem32, reg3211 /rAdd reg32 to reg/mem32 + CF.
+// ADC reg/mem64, reg6411 /rAdd reg64 to reg/mem64 + CF.
+// ADC reg8, reg/mem812 /rAdd reg/mem8 to reg8 + CF.
+// ADC reg16, reg/mem1613 /rAdd reg/mem16 to reg16 + CF.
+// ADC reg32, reg/mem3213 /rAdd reg/mem32 to reg32 + CF.
+// ADC reg64, reg/mem6413 /rAdd reg/mem64 to reg64 + CF.
 
 // PUSH reg64   50 +rq
 const PUSH_R64: Instruction = Instruction {
